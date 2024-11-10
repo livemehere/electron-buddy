253e692d91f811403192632dc56e23edca934112
livemehere <rhdxoals@gmail.com>
2024-11-11 00:51:45 +0900

    feat: vite root set renderer

--- packages/vite-plugin-electron-kit/src/index.ts
+++ packages/vite-plugin-electron-kit/src/index.ts
@@ -1,7 +1,8 @@
-import {PluginOption, ViteDevServer, WebSocket} from "vite";
+import {PluginOption, WebSocket} from "vite";
 import buildBundle from "./utils/buildBundle";
 import {runApp} from "./utils/runApp";
 import AddressInfo = WebSocket.AddressInfo;
+import {ChildProcess} from "child_process";
 
 type Options = {
     main:{
@@ -16,31 +17,37 @@ type Options = {
 }
 
 export function electron(options?:Options):PluginOption[]{
+    let app:ChildProcess|null = null;
+
     return [
         {
             name:'electron-kit-renderer',
             config:()=>({
+                root:'./renderer',
                 base:'',
                 build:{
                     emptyOutDir:false,
-                    outDir: './dist/renderer',
-                    rollupOptions:{
-                        input:{
-                            app: './renderer/index.html'
-                        }
-                    }
+                    outDir: '../dist/renderer',
                 }
-            }),
-        },
+            }
+        )},
         {
             name:'electron-kit-main',
             configureServer(server){
                 server.httpServer?.once('listening',async ()=>{
                     const address = server.httpServer?.address() as AddressInfo;
-                    process.env['RENDERER_URL'] = `http://localhost:${address.port}/renderer/index.html`;
-                    await buildBundle('./main/index.ts', './dist','main.js')
-                    await buildBundle('./preload/index.ts','./dist','preload.js')
-                    await runApp();
+                    process.env['RENDERER_URL'] = `http://localhost:${address.port}`;
+
+                    await buildBundle('./preload/index.ts','./dist','preload.js',()=>{
+                        server.ws.send({type:'full-reload'});
+                    },true)
+                    await buildBundle('./main/index.ts', './dist','main.js',async ()=>{
+                        if(app){
+                            app.kill();
+                        }
+                        app = await runApp();
+                    },true)
+
                 })
             }
         },
