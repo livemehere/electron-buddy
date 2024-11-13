fab9f630e81bae1dd074efa1f7c88fffd290a4fd
livemehere <rhdxoals@gmail.com>
2024-11-13 22:31:48 +0900

    restore playground

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: './renderer',
-  plugins: [
-    electron({
-      outDir: './out',
-      preload: {
-        entry: './preload2/index.ts'
-      },
-      main: {
-        entry: './main2/index.ts'
-      }
-    }),
-    react()
-  ]
+  plugins: [electron(), react()]
 });
